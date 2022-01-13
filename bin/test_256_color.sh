9abbee325e1f2a73367c194d51448fa802a0936e
David Stark <david.stark@doddle.com>
2022-01-13 00:53:41 +0000

    missed a few

---
+++ bin/test_256_color.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+###############################################################################
+# Author:      David Stark
+# Vim:         ts=4 et sw=4 sts=4
+# Notes:       
+###############################################################################
+set -o nounset                              # Treat unset variables as an error
+
+#!/bin/bash
+awk 'BEGIN{
+    s="/\\/\\/\\/\\/\\"; s=s s s s s s s s s s s s s s s s s s s s s s s;
+    for (colnum = 0; colnum<256; colnum++) {
+        r = 255-(colnum*255/255);
+        g = (colnum*510/255);
+        b = (colnum*255/255);
+        if (g>255) g = 510-g;
+        printf "\033[48;2;%d;%d;%dm", r,g,b;
+        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
+        printf "%s\033[0m", substr(s,colnum+1,1);
+    }
+    printf "\n";
+}'
+
+
