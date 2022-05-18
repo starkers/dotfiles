9623abd33225de7f4c2fb31142a09827a7daa47e
David Stark <david@starkers.org>
2022-05-18 13:16:52 +0100

    basic vim+pylsp

---
+++ config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,34 @@
+# https://github.com/justinmayer/virtualfish
+# 
+# using an asdf python:
+
+# python -m pip install virtualfish
+# asdf reshim python
+# 'vf install'  (created this file)
+# I've extended it to have some vars thats all
+
+set ASDF_PY_PATH $HOME/.asdf/installs/python
+set ASDF_PY_VER_FULL 3.8.8
+set ASDF_PY_VER_MINOR 3.8
+
+
+if test -d $ASDF_PY_PATH/$ASDF_PY_VER_FULL/bin
+  set -g VIRTUALFISH_VERSION 2.5.4
+  set -g VIRTUALFISH_PYTHON_EXEC $ASDF_PY_PATH/$ASDF_PY_VER_FULL/bin/python
+  source $ASDF_PY_PATH/$ASDF_PY_VER_FULL/lib/python$ASDF_PY_VER_MINOR/site-packages/virtualfish/virtual.fish
+  emit virtualfish_did_setup_plugins
+end
+
+
+
+
+# To create a new virtual environment use vf new
+#
+#  vf new my_new_env
+#
+# # If you want create a new python3 environment specify it via -p flag
+#
+#  vf new -p python3 my_new_env
+#
+
+# 
---
+++ config/nvim/lua/user/lsp/settings/pylsp.lua
@@ -0,0 +1,7 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#pylsp
+return {
+  settings = {
+  },
+
+}
+
