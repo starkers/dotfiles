ce07f56ae6aed59d4cd01f27b4bffcc85bd861fc
David Stark <david@starkers.org>
2022-10-18 17:18:30 +0100

    including gotop and babelfish in my defaults

--- tweaks/20-golang.sh
+++ tweaks/20-golang.sh
@@ -2,14 +2,14 @@
 
 ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
-source ${ROOTDIR}/shell-functions
+source "${ROOTDIR}/shell-functions"
 declare -a deps=(
 	go
 	git
 )
 
 for dep in "${deps[@]}"; do
-	if ! executableInPath ${dep}; then
+	if ! executableInPath "${dep}"; then
 		die "please install '${dep}' to continue"
 	fi
 done
@@ -17,12 +17,14 @@ done
 #############################################################
 
 declare -a globalPlugins=(
+	bou.ke/babelfish@latest # https://github.com/bouk/babelfish
 	# github.com/starkers/ggg@latest
 	golang.org/x/tools/cmd/goimports@latest
 	#github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.0
+	github.com/xxxserxxx/gotop/v4/cmd/gotop@latest
 )
 
-for data in ${globalPlugins[@]}; do
-	say installing $data
-	try go install -x ${data}
+for data in "${globalPlugins[@]}"; do
+	say installing "${data}"
+	try go install -x "${data}"
 done
