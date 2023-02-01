382f5c794262059ee5e154384288f15fee68e308
David Stark <david@starkers.org>
2023-02-01 12:00:48 +0000

    ranger config

---
+++ config/fish/conf.d/00-rancher-desktop.fish
@@ -0,0 +1,15 @@
+# vi: ft=fish
+#
+#
+
+set NEWPATH $HOME/.rd/bin
+if test -d $NEWPATH
+  fish_add_path -a $NEWPATH
+end
+
+# I vendor my own kubectl "thanks"
+if test -f $NEWPATH/kubectl
+  rm $NEWPATH/kubectl
+end
+
+
