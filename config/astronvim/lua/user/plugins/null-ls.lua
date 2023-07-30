fa29ccf2179d37e83d1eb169ff0bc4fa7234f117
David Stark <david@starkers.org>
2023-07-30 11:28:07 +0100

    some extra formatting for null-ls

--- config/astronvim/lua/user/plugins/null-ls.lua
+++ config/astronvim/lua/user/plugins/null-ls.lua
@@ -1,33 +1,37 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  opts = function(_, opts)
-    local null_ls = require("null-ls")
-    opts.sources = {
+	"jose-elias-alvarez/null-ls.nvim",
+	opts = function(_, opts)
+		local null_ls = require("null-ls")
+		opts.sources = {
 
-      null_ls.builtins.formatting.prettier,
-      null_ls.builtins.formatting.isort,
-      null_ls.builtins.formatting.goimports,
-      null_ls.builtins.formatting.terraform_fmt,
-      -- null_ls.builtins.formatting.clang_format,
-      -- null_ls.builtins.formatting.rustfmt,
+			-- TODO: stop prettier formatting yaml files?
+			-- null_ls.builtins.formatting.prettier,
+			null_ls.builtins.formatting.isort,
+			null_ls.builtins.formatting.goimports,
+			null_ls.builtins.formatting.terraform_fmt,
+			null_ls.builtins.formatting.shfmt,
+			null_ls.builtins.formatting.fish_indent,
+			-- null_ls.builtins.formatting.clang_format,
+			-- null_ls.builtins.formatting.rustfmt,
 
-      -- null_ls.builtins.formatting.shfmt.with {
-      --   args = { "-i", "2" },
-      --
-      -- },
+			-- null_ls.builtins.formatting.shfmt.with {
+			--   args = { "-i", "2" },
+			--
+			-- },
 
-      -- python
-      null_ls.builtins.formatting.black,
-      null_ls.builtins.diagnostics.flake8,
-      -- null_ls.builtins.diagnostics.pylint,
-      -- null_ls.builtins.diagnostics.mypy,
+			-- python
+			null_ls.builtins.formatting.black,
+			null_ls.builtins.diagnostics.flake8,
+			null_ls.builtins.diagnostics.fish,
+			-- null_ls.builtins.diagnostics.pylint,
+			-- null_ls.builtins.diagnostics.mypy,
 
-      -- lua
-      null_ls.builtins.diagnostics.luacheck,
-      null_ls.builtins.formatting.stylua,
-    }
-    return opts
-  end,
+			-- lua
+			null_ls.builtins.diagnostics.luacheck,
+			null_ls.builtins.formatting.stylua,
+		}
+		return opts
+	end,
 }
 
 -- return function(config) -- overrides `require("null-ls").setup(config)`
