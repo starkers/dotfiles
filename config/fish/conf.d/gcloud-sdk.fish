c75fa6ee683916648071fc04b14e884e3c7cf046
David Stark <david@starkers.org>
2023-11-20 12:33:11 +0000

    gcloud SDK fix for arch

--- config/fish/conf.d/gcloud-sdk.fish
+++ config/fish/conf.d/gcloud-sdk.fish
@@ -4,3 +4,12 @@ set INC_FILE $HOME/.google-cloud-sdk/path.fish.inc
 if test -f $INC_FILE
     . $INC_FILE
 end
+
+# https://git.polarian.dev/AUR/google-cloud-cli/pulls/10#issuecomment-291
+if test -d /opt/google-cloud-cli
+    set CLOUDSDK_ROOT_DIR /opt/google-cloud-cli
+    set GOOGLE_CLOUD_SDK_HOME $CLOUDSDK_ROOT_DIR
+    set CLOUDSDK_PYTHON /usr/bin/python
+    set CLOUDSDK_PYTHON_ARGS -S
+    fish_add_path $CLOUDSDK_ROOT_DIR/bin
+end
