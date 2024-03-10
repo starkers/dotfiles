049594789d517a24c6db4d29226e69ca2d84e99d
David Stark <david@starkers.org>
2024-03-10 06:48:28 +0000

    chore: npm bumped, annoying fnm stdout suppressed

--- config/fish/conf.d/fnm.fish
+++ config/fish/conf.d/fnm.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     if command -s fnm >/dev/null
+        set -gx FNM_LOGLEVEL quiet
         fnm env --use-on-cd --shell fish | source
 
         # add auto-complete
@@ -9,6 +10,7 @@ if status is-interactive
         if test "$TERM_PROGRAM" = vscode
             _fnm_autoload_hook
         end
+        # suppress fnm telling me stuff, if I wanna know I'll use direnv to override this
     else
         echo '#WARN: fnm not installed'
     end
