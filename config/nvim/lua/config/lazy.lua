7aa50309af785f945c2d90abebbd84769ec05d18
David Stark <david@starkers.org>
2024-07-04 15:02:40 +0100

    nvim bump

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -7,6 +7,9 @@ end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
+  -- NOTE: See Lazy config doc...  https://lazy.folke.io/configuration
+  --
+  -- concurrency = 1, -- use this on shitty wifi
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
