46d3d4087c8bf0f10ff3bcc53144cdd6f6ac8e68
David Stark <david@starkers.org>
2025-05-21 16:08:15 +0100

    nvim (lazy) bumps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -25,11 +25,6 @@ vim.keymap.set("n", "<c-Right>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 
 -- ctrl+l is a standard shortcut to clear a terminal.. please ignore it in terminal windows
 -- in fact scrap all the "hjkl" ones.. I'm not gonna use em
-vim.keymap.del("t", "<c-h>")
-vim.keymap.del("t", "<c-j>")
-vim.keymap.del("t", "<c-k>")
-vim.keymap.del("t", "<c-l>")
--- instead of Control+/  use Control+\ for terminal
 local Util = require("lazyvim.util")
 local lazyterm = function()
   Util.terminal(nil, { cwd = Util.root() })
@@ -44,7 +39,7 @@ vim.keymap.set("n", "<c-_>", lineWise, { desc = "Comment linewise" })
 vim.keymap.set("t", "<c-_>", lineWise, { desc = "Comment linewise" })
 vim.keymap.set("v", "<c-_>", lineWise, { desc = "Comment linewise" })
 
--- vim.keymap.set("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
+vim.keymap.set("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
 -- vim.keymap.set("n", "<c-\\>", lazyterm, { desc = "Terminal (root dir)" })
 
 vim.keymap.set("n", "<F5>", lazyterm, { desc = "Terminal (root dir)" })
--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -5,6 +5,7 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.g.lazyvim_check_order = false
 
 require("lazy").setup({
   -- NOTE: See Lazy config doc...  https://lazy.folke.io/configuration
