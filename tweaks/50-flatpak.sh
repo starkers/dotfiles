6166411ae4994e07055e0b4831377b59cecf5fc8
David Stark <david@starkers.org>
2026-03-27 10:41:18 +0000

    updated flatpak list

--- tweaks/50-flatpak.sh
+++ tweaks/50-flatpak.sh
@@ -14,17 +14,18 @@ for dep in "${deps[@]}"; do
 done
 
 declare -a flathub=(
+  net.cozic.joplin_desktop
   com.jetpackduba.Gitnuro
-  com.bitwarden.desktop
+  # com.bitwarden.desktop
   # com.discordapp.Discord
   # de.shorsh.discord-screenaudio
-  com.slack.Slack
-  com.spotify.Client
+  # com.slack.Slack
+  dev.aunetx.deezer
   # dev.alextren.Spot
-  us.zoom.Zoom
+  # us.zoom.Zoom
   # com.jetbrains.GoLand
   # com.mattjakeman.ExtensionManager
-  com.github.IsmaelMartinez.teams_for_linux
+  # com.github.IsmaelMartinez.teams_for_linux
 )
 
 try flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
